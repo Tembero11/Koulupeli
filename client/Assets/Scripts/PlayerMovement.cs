232216e89968adef701562f22825ec74628a1d52
AhmetHyseni <ahmetfin4b@gmail.com>
2023-04-20 17:49:27 +0300

    Pelaaja, Rata ja Esteet toimivat

---
+++ client/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    bool alive = true;
+
+    public float speed = 5;
+    public Rigidbody rb;
+
+    float horizontalInput;
+    public float horizontalMultiplier = 2;
+
+    private void FixedUpdate()
+    {
+        if (!alive) return;
+
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (transform.position.y < -5)
+        {
+            Die();
+        }
+        if (transform.position.x < -4)
+        {
+            Die();
+        }
+        if (transform.position.x > 4)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        // K‰ynnist‰‰ pelin uudelleen
+        Invoke("Restart", 2);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
