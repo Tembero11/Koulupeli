613f8abcbdc8618bd1a2d938fd5613d6d8c5bf9e
AhmetHyseni <ahmetfin4b@gmail.com>
2023-06-01 14:36:34 +0300

    Nimi

--- client/Assets/Scripts/GroundSpawner.cs
+++ client/Assets/Scripts/GroundSpawner.cs
@@ -2,19 +2,32 @@ using UnityEngine;
 
 public class GroundSpawner : MonoBehaviour
 {
-    public GameObject groundTile;
+    [SerializeField] GameObject groundTile;
     Vector3 nextSpawnPoint;
     //Rakentaa/Spawnaa Tilet
-    public void SpawnTile()
+    public void SpawnTile(bool spawnItems)
     {
         GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
         nextSpawnPoint = temp.transform.GetChild(1).transform.position;
+
+        if (spawnItems)
+        {
+            temp.GetComponent<GroundTile>().SpawnObstacle();
+            temp.GetComponent<GroundTile>().SpawnCoins();
+        }
     }
     private void Start()
     {
         for (int i = 0; i < 15; i++)
         {
-            SpawnTile();
+            if (i < 2)
+            {
+                SpawnTile(false);
+            }
+            else
+            {
+                SpawnTile(true);
+            }
         }
     }
 }
